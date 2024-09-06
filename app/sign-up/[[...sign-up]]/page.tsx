9966f40817b86227329da331d460e6f5f31f0e47
Ogwu Emmanuel Berit <beritogwu@gmail.com>
2024-09-06 13:48:30 +0100

    fixed

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <SignUp
-        fallbackRedirectUrl={process.env.NEXT_PUBLIC_BASE_URL}
+        forceRedirectUrl={process.env.NEXT_PUBLIC_BASE_URL}
         afterSignOutUrl={`${process.env.NEXT_PUBLIC_BASE_URL}/sign-in`}
       />
     </div>
